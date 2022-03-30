f63e5ba70c3f3d11f1383709df4ad0e9308b1d8e
Sive <sisandla@student.wethinkcode.co.za>
2022-03-30 12:55:45 +0200

    update some of the files

--- crowdfunding/settings.py
+++ crowdfunding/settings.py
@@ -128,13 +128,9 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
 STATIC_URL = 'static/'
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # media uploading
