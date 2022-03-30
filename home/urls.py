7b2aaf065cbc1c292419023a67df71f7b3074b33
Sive <sisandla@student.wethinkcode.co.za>
2022-03-30 14:34:50 +0200

    Update files

--- home/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import include, path
-from django.contrib import admin
-from . import views
-from home.views import HomeView, CategoryView
-
-urlpatterns = [
-    path('', HomeView.as_view(), name='home'),
-    path('home/category/<int:category_id>', CategoryView.as_view(), name='category')
-]
